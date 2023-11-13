f7eb7ff60e7e7659598dca98071f8f84b089c17e
Hafizur Rahman <123953804+Hafizur-Rahman-SD@users.noreply.github.com>
2023-11-13 10:18:13 +0600

    Update Take user input.c

--- Take user input.c
+++ Take user input.c
@@ -2,15 +2,11 @@
 //This code for know how to take input from User.
 
 #include<stdio.h>
-
 int main(){
   
 int age;
-  
 printf("Enter your age:"\n); //we can write here what we want to print 
-  
 scanf("%d",&age);
-  
 Printf("My Age is: %d",age);
   
 retuen 0;
