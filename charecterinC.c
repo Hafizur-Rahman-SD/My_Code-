411d52e2fc70e4c36724e1772f0d16d14ca12e9c
Hafizur Rahman <123953804+Hafizur-Rahman-SD@users.noreply.github.com>
2023-12-25 17:36:10 +0600

    Update charecterinC.c

--- charecterinC.c
+++ charecterinC.c
@@ -1,7 +1,8 @@
 //Test code done. 
+
 #include<stdio.h>
 int main(){
-char ='H';  //this our diffindable char variablle.
+char ='H';  //this our diffindable character  variable.
 char ='A';
 printf("Result is: %c\n",char);
     return 0;
