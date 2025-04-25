064af7e09c7acc734f0c7debada85d07e3ba84bf
Hafizur Rahman <123953804+Hafizur-Rahman-SD@users.noreply.github.com>
2025-04-25 10:58:27 +0600

    Update return_Printf_Function_Value.c

--- return_Printf_Function_Value.c
+++ return_Printf_Function_Value.c
@@ -1,10 +1,12 @@
 //printf() function return. here we can undrastand what is the return i ptintf() function.
 #include <stdio.h>
 int main(){
+  
 int a ;
 scanf("%d",&a);
 int d=printf("Input is =%d",a);
 printf("%d",d);
 return 0;
+  
 }
 
